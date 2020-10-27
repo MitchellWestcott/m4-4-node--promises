ff40f8aa391650534993a0cb8f013eb8d95916d9
MitchellWestcott <westcott.mitchell@gmail.com>
2020-10-27 10:52:10 -0400

    m4-4 complete

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,14 +1,32 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-const complicatedArray = ['cucumber', 44, true];
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
 
 const makeAllCaps = (array) => {
   // write some code
+  myPromise = new Promise((resolve, reject) => {
+    if (array.every((item) => typeof item === "string")) {
+      const arrayToCaps = array.map((i) => i.toUpperCase());
+      resolve(arrayToCaps);
+    } else {
+      reject(`Array contains non-string values`);
+    }
+  });
+  return myPromise;
 };
 
 const sortWords = (array) => {
   // write some code
+  myOtherPromise = new Promise((resolve, reject) => {
+    if (array.every((item) => typeof item === "string")) {
+      const arraySorted = array.sort();
+      resolve(arraySorted);
+    } else {
+      reject(`Array contains non-string values`);
+    }
+  });
+  return myOtherPromise;
 };
 
 // Calling (testing)
--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -1,18 +1,21 @@
 // Exercise 3.2 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+require("dotenv").config();
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: "ab5f9ab018334b1fb12a55ee522021e5",
+    q: address,
   };
 
   // return something...
+  return opencage.geocode(requestObj).then((data) => data.results[0].geometry);
 };
 
 getPositionFromAddress(
-  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
+  "1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8"
 ).then((response) => console.log(response));
+
+module.exports = { getPositionFromAddress };
